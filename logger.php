d02548017ff1d5c7a2d41c8f9d1c5807e0cca918
Gayrat Usmonov <rabdavinci@yandex_at_ru>
2020-07-01 18:27:56 +0500

    Code improved, added Status code

--- logger.php
+++ logger.php
@@ -1,61 +1,55 @@
 <?php
 
-function setLog() {
-	//$_SERVER['QUERY_STRING']);
-	echo $HTTP_RAW_POST_DATA;
-	// Collect data
-	$date  =  date("Y-m-d H:i:s");
-	$filename = 'log.txt';
-	
-	$content = "Date: ".$date.PHP_EOL;
-	
-	$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$filename = 'log.txt';
 
-	$content .= "URL: ".$url.PHP_EOL;
-	
-	$data = getData();
-	$content .= $data.PHP_EOL;
-	
-	$content .= "---------------------------------------".PHP_EOL;
-	// Save to  file 
-	file_put_contents($filename, $content, FILE_APPEND | LOCK_EX);
-	
-	// Print. Tips: replace PHP_EOL with <br>
-	echo str_replace(array("\r\n", "\r", "\n"), '<br>', $content);
+$date  =  date( "Y-m-d H:i:s" );
+$content = "Date: " . $date . PHP_EOL;
+
+// Handling headers
+$content .= "Headers:" . PHP_EOL;
+
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$content .= "Request URL: " . $url . PHP_EOL;
+
+$statusCode = http_response_code();
+$content .= "Status Code: " . $statusCode . PHP_EOL;
+
+$headers = get_request_headers_as_string();
+$content .= $headers;
+
+// Handling raw GET params
+if(!empty($_GET)) {
+  $content .= "Query String Parametres: " . PHP_EOL;
+  $content .= $_SERVER['QUERY_STRING'] . PHP_EOL;
 }
 
-function getData()
-{
-	$result = "";
-	
-	// Handling headers
-	$result .= "Headers:".PHP_EOL;
-	$headers = get_request_headers();
-	$result .= $headers;
-	
-	// Handling raw GET params
-	if(!empty($_GET)) {
-		$result .= "Raw GET: ".$_SERVER['QUERY_STRING'].PHP_EOL;
-	}
-	
-	// Handling raw POST params
-	if(!empty($_POST)) {
-		$result .= "Raw POST: ".file_get_contents("php://input").PHP_EOL;
-	}
-	
-	return $result;
+// Handling raw POST params
+if(!empty($_POST)) {
+  $content .= "Form Data: " . PHP_EOL;
+  $content .= file_get_contents("php://input") . PHP_EOL;
 }
 
-function get_request_headers() {
-    $headers = "";
-    foreach($_SERVER as $key => $value) {
-        if(strpos($key, 'HTTP_') === 0) {
-			$headers .= str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5))))).":".$value.PHP_EOL;
-        }
+$content .= "--------------------------------" . PHP_EOL;
+
+// Save to  file 
+file_put_contents($filename, $content, FILE_APPEND | LOCK_EX);
+
+// Print
+echo str_replace(array("\r\n", "\r", "\n"), '<br>', $content);
+
+/**
+ * Parse headers from $_SERVER
+ * @return string 
+ */
+function get_request_headers_as_string() {
+  $headers = "";
+  foreach($_SERVER as $key => $value) {
+    if(strpos($key, 'HTTP_') === 0) {
+      $headers .= str_replace(' ', '-', ucwords(str_replace('_', ' ', 
+                strtolower(substr($key, 5))))).":".$value . PHP_EOL;
     }
-    return $headers;
+  }
+  return $headers;
 }
-
-setLog();
 
 ?>
